fd155bd6c3afaa467940347f9c7f957afcfc8afe
BlackBird33 <Jusharra@users.noreply.github.com>
2025-07-29 17:05:53 -0700

    Add Link import to FAQs.tsx

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ChevronDown, HelpCircle, DollarSign, Users, Shield, Calendar } from 'lucide-react';
 
 const FAQs: React.FC = () => {
